a0004b96dbcd38e7e77b96792c9104ca2a0a7a7b
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2022-08-29 21:13:53 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 public class Main{
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str," ");
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
         System.out.println(a+b);
     }
 }
