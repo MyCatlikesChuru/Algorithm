01f4c5073c66866bfdedcada8b178880f341dfc6
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2022-09-11 14:25:08 +0900

    [Bronze V] Title: A+B - 8, Time: 164 ms, Memory: 14788 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        for(int i=1;i<=num;i++){
+            String[] strs = br.readLine().split(" ");
+            int a = Integer.parseInt(strs[0]);
+            int b = Integer.parseInt(strs[1]);
+            bw.write(String.format("Case #%d: %d + %d = %d"+"\n",i,a,b,a+b));
+        }
+        bw.flush();
+        bw.close();
+    }
+}
