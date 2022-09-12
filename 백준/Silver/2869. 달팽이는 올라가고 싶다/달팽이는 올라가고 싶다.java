3469de0e9413d7001227dc86c4da32cbfab61978
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2022-09-12 23:14:41 +0900

    Update 달팽이는 올라가고 싶다.java

--- 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,23 +1,18 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-		int up = Integer.parseInt(st.nextToken());
-		int down = Integer.parseInt(st.nextToken());
-		int length = Integer.parseInt(st.nextToken());
- 
-		int day = (length - down) / (up - down);
-		if ((length - down) % (up - down) != 0)
-			day++;
- 
-		System.out.println(day);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] strs = br.readLine().split(" ");
+        int A = Integer.parseInt(strs[0]); // 낮에 올라가는 미터
+        int B = Integer.parseInt(strs[1]); // 밤에 떨어지는 미터
+        int V = Integer.parseInt(strs[2]); // 올라가야할 목적지
+        int days;
+        days = (V-B)/(A-B);
+        if((V-B)%(A-B)!=0)days++;
+        bw.write(String.valueOf(days));
+        bw.flush();
+        bw.close();
+    }
 }
