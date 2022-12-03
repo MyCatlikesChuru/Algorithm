a57467ea7d9a001fc7e3c90f311a594b476385b0
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2022-12-03 23:12:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.19 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        String answer = "";
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        for(String player : participant){
+            hashMap.put(player, hashMap.getOrDefault(player,0) + 1);
+        }
+
+        for(String player : completion){
+            hashMap.put(player, hashMap.get(player) - 1);
+        }
+
+        for(String key : hashMap.keySet()){
+            if(hashMap.get(key) != 0){
+                answer = key;
+            }
+        }
+        return answer;
+    }
+}
