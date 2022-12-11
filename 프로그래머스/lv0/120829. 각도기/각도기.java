a2c5cf57c09ba6c4738a574663ac4c70d94c32e7
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2022-12-11 16:24:57 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int angle) {
+        
+        if(angle == 90) return 2;
+        else if(angle == 180) return 4;
+        else if(angle < 90) return 1;
+        
+        
+        return 3;
+    }
+}
