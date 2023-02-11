7b87d6e40bd9a5bcb93f249441ae27f0a26bb00d
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2023-02-12 01:37:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 114.35 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,18 @@
 import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Map<String, Integer> hashMap = new HashMap<>();
+        // 참가자를 카운팅으로 그룹핑 시켜 Map으로 반환
+        Map<String, Long> hashMap = Arrays.stream(participant)
+                .collect(Collectors.groupingBy(person -> person, Collectors.counting()));
 
-        // 참가자 체크 (동명인이 있을 경우 value값 1씩 증가)
-        Arrays.stream(participant)
-                .forEach(person -> hashMap.put(person, hashMap.getOrDefault(person,0)+1));
-
-        // 완주자 체크 (참가자 중 완주한 사람은 value값을 1씩 감소)
+        // 완주자 목록으로 참가자를 비교해 Map에서 제거하기
         Arrays.stream(completion)
-                .forEach(person -> hashMap.put(person, hashMap.get(person)-1));
-
-        // 값이 0이 아닐경우는 완주하지 못한 것으로 간주
-        for(String key : hashMap.keySet()){
-            if(hashMap.get(key) != 0){
-                answer = key;
-            }
-        }
-        return answer;
+                .forEach(person -> {
+                            if (hashMap.get(person) == 1L) hashMap.remove(person);
+                            else hashMap.put(person, hashMap.get(person) - 1);
+                        });
+        return hashMap.keySet().iterator().next();
     }
 }
