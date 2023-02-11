20a64ec2e59b1b89a6e04c3bb76d9451987a16dd
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2023-02-11 18:47:43 +0900

    [level 1] Title: 폰켓몬, Time: 3.00 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -3,14 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         
-        int answer = 0;
-        int half = (int) nums.length / 2 ;
+        int max = nums.length / 2 ;
         Set<Integer> hashSet = new HashSet<>();
-        
-        for(Integer num : nums){
-            hashSet.add(num);
-        }
-        
-        return (hashSet.size() > half) ? half : hashSet.size();
+        Arrays.stream(nums).forEach(num -> hashSet.add(num));
+        return (hashSet.size() > max) ? max : hashSet.size();
     }
 }
