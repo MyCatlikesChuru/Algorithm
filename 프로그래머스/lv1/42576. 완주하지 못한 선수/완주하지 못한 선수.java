4770bb256a1b5e1034985d9a2f4202a507e894e6
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2023-02-11 23:54:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 100.82 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,14 +4,20 @@ class Solution {
         String answer = "";
         Map<String, Integer> hashMap = new HashMap<>();
 
+        // 참가자 체크 (동명인이 있을 경우 value값 1씩 증가)
         Arrays.stream(participant)
                 .forEach(person -> hashMap.put(person, hashMap.getOrDefault(person,0)+1));
+
+        // 완주자 체크 (참가자 중 완주한 사람은 value값을 1씩 감소)
         Arrays.stream(completion)
                 .forEach(person -> hashMap.put(person, hashMap.get(person)-1));
+
+        // 값이 0이 아닐경우는 완주하지 못한 것으로 간주
         for(String key : hashMap.keySet()){
-            if(hashMap.get(key) != 0) answer = key;
+            if(hashMap.get(key) != 0){
+                answer = key;
+            }
         }
         return answer;
-    
     }
 }
