a9a2863f0a89dd3d4b3b3d4d69ba545308d9efa2
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2023-02-12 18:21:51 +0900

    [level 2] Title: 전화번호 목록, Time: 276.20 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,20 +4,11 @@ import java.util.stream.Collectors;
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        var ref = new Object() {
-            String startsWith = phone_book[0];
-        };
-        //
-        boolean result = Arrays.stream(phone_book)
-                .filter(s -> !s.equals(ref.startsWith))
-                .map(phone -> {
-                    if (phone.startsWith(ref.startsWith)) return "false";
-                    ref.startsWith = phone;
-                    return "true";
-                })
-                .collect(Collectors.toList())
-                .contains("false");
-
-        return !result;
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
+            }
+        }
+        return true;
     }
 }
