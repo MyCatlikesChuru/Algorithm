2fe480f642ae94051c53b021e0348103220ff325
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2023-03-03 15:59:38 +0900

    [level 1] Title: K번째수, Time: 0.46 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        // commands의 [i,j,k] 원소를 가진다.
+        // 반환할 배열을 만든다.
+        int[] answer = new int[commands.length];
+
+        // commands 배열 길이만큼 순회를 한다.
+        for (int i=0; i<commands.length; i++) {
+            int[] cutArray = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(cutArray);
+            answer[i] = cutArray[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+}
