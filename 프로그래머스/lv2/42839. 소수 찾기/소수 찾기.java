b65a4b64d726cf36bd61219de988dd62fce84633
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2023-03-12 17:36:04 +0900

    [level 2] Title: 소수 찾기, Time: 2.00 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -31,12 +31,10 @@ class Solution {
     }
 
     public boolean isPrime(int num) {
-        int sqrt = (int) Math.sqrt(num);
-        if (num == 1) return false;
         if (num == 2) return true;
-        if (num % 2 == 0) return false;
-        for(int i=3; i<=sqrt;i++){
-            if(num%i==0){
+        if (num % 2 == 0 || num == 1) return false;
+        for (int i = 3; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
