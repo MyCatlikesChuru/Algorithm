bc34f1498529f0f73d0fb557adc7572e4216234a
Lee Jaehyeok <95069395+MyCatlikesChuru@users.noreply.github.com>
2023-03-15 23:05:34 +0900

    [level 2] Title: 소수 찾기, Time: 3.12 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(String numbers) {
+
         // 만들수 있는 조합 중 소수의 갯수를 구하는 문제
         // String 길이가 달라지기 때문에 재귀로 풀어야하는 문제
         String[] arr = numbers.split("");
         boolean[] visit = new boolean[arr.length];
-        Set<Integer> combination =  new HashSet<>();
+        Set<Integer> answer = new HashSet<>();
 
         for (int i = 0; i < arr.length; i++) {
-            recursion(arr, arr[i], combination, i, visit);
+            recursion(arr, arr[i], answer, i, visit);
             visit[i] = false;
         }
-
-        return combination.size();
+        return answer.size();
     }
-    
-     public void recursion(String[] numbers, String number, Set<Integer> combination, int index, boolean[] visit) {
+
+    public void recursion(String[] arr, String number, Set<Integer> answer, int index, boolean[] visit) {
         visit[index] = true; // 해당 index 재귀 호출시 방문 체크
         int num = Integer.parseInt(number);
-        if(isPrime(num)) combination.add(num);
-        if(number.length() == numbers.length) return;
+        if (isPrime(num)) answer.add(num);
+        if (number.length() == arr.length) return;
 
-        for (int i = 0; i < numbers.length; i++) {
-            if(!visit[i]) {
-                recursion(numbers, number + numbers[i],combination, i, visit);
+        for (int i = 0; i < arr.length; i++) {
+            if (!visit[i]) {
+                recursion(arr, number + arr[i], answer, i, visit);
                 visit[i] = false;
             }
         }
